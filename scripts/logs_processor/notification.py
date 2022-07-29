8a5a2057d1019bdb1a82c68a2ae95ef22f0ec3f9
Matthew Palmer <71637682+mpalmer23@users.noreply.github.com>
2022-07-29 16:15:47 +0100

    fix: added more testing packages

--- scripts/logs_processor/notification.py
+++ scripts/logs_processor/notification.py
@@ -46,3 +46,6 @@ if __name__ == "__main__":
 ####### COMMAND TO RUN IN TERMINAL TO RUN THIS FILE WITH MYPY TESTING (VARIABLE-TYPE-CHECKER) --> "mypy scripts\logs_processor\notification.py"
 ####### COMMAND TO RUN IN TERMINAL TO RUN THIS FILE WITH PYLINT TESTING (FORMATTING/GENERAL CODE CHECKER) --> "python -m pipenv run pylint .\scripts\logs_processor\notification.py"
 ####### COMMAND TO RUN IN TERMINAL TO RUN THIS FILE WITH PYFLAKES TESTING (FORMATTING/GENERAL CODE CHECKER, BUT GIVES LESS FALSE POSITIVES THAN PYLINT) --> "python -m pipenv run pyflakes .\scripts\logs_processor\notification.py"
+####### COMMAND TO RUN IN TERMINAL TO RUN THIS FILE WITH FLAKE8 TESTING (WRAPPER PACKAGE THAT COMBINES PYFLAKES WITH SOME OTHER LINTERS) --> "python -m pipenv run flake8 .\scripts\logs_processor\notification.py"
+####### COMMAND TO RUN IN TERMINAL TO RUN THIS FILE WITH PROSPECTOR TESTING (WRAPPER PACKAGE THAT COMBINES PYFLAKES WITH SOME OTHER LINTERS, BUT EASILY CUSTOMISABLE TO SUPPRESS UNIMPORTANT WARNINGS) --> "python -m pipenv run prospector .\scripts\logs_processor\notification.py"
+####### COMMAND TO RUN IN TERMINAL TO RUN THIS FILE WITH BANDIT TESTING (TESTS FOR SECURITY VULNERABILITIES) --> "python -m pipenv run bandit .\scripts\logs_processor\notification.py"
